e6d121f653257253967d3ff287f076a24d1d2878
IshaKhatana-0 <khatanaishaa@gmail.com>
2025-03-20 12:09:26 +0530

    cpp programs

--- patternn.cpp
+++ patternn.cpp
@@ -57,18 +57,19 @@ int main(){
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    for(int i=1;i<=5;i++){
-        for(int j =1;j<=i;j++){
-            if((i%2!=0)and(j%2!=0))
-            cout<<'0';
-            else cout<<'1';
-
-            if((i%2==0)and(j%2!=0)) cout<<'1';
-            else cout<<'0';
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<5-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
         }
         cout<<endl;
     }
 }
+
+
+
 
 
 
